be07eb7e2551f929fc8cf86a28320b6f5d84e973
Elijah Flader <elijahflader@gmail.com>
2026-05-18 15:54:11 -0400

    Get instrument name

---
+++ src/main.py
@@ -0,0 +1,14 @@
+import pyvisa
+
+rm = pyvisa.ResourceManager('@py')
+inst = rm.open_resource('ASRL/dev/cu.usbserial-1130::INSTR')
+
+inst.baud_rate = 115200  # XDM2041 uses 115200, not 9600
+inst.data_bits = 8
+inst.stop_bits = pyvisa.constants.StopBits.one
+inst.parity = pyvisa.constants.Parity.none
+inst.timeout = 3000
+inst.read_termination = '\n'
+inst.write_termination = '\n'
+
+print(inst.query('*IDN?'))
