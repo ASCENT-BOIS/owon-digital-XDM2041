c1003dc6027b24777854a5b2d66c1c810662652c
Elijah Flader <elijahflader@gmail.com>
2026-05-19 11:11:43 -0400

    Add the test files for VISA

---
+++ src/visaTest.py
@@ -0,0 +1,23 @@
+import pyvisa
+import time
+
+rm = pyvisa.ResourceManager('@py')
+inst = rm.open_resource('ASRL/dev/cu.usbserial-1130::INSTR')
+
+inst.baud_rate = 115200  # XDM2041 uses 115200, not 9600
+inst.data_bits = 8
+inst.stop_bits = pyvisa.constants.StopBits.one
+inst.parity = pyvisa.constants.Parity.none
+inst.timeout = 3000
+inst.read_termination = '\n'
+inst.write_termination = '\n'
+inst.flow_control = pyvisa.constants.VI_ASRL_FLOW_RTS_CTS
+
+print(inst.query('*IDN?'))
+
+print(inst.query('FUNC?'))
+
+inst.write('SYST:REM')
+time.sleep(0.5)  # longer delay
+inst.write('CONF:RES')
+time.sleep(0.2)
