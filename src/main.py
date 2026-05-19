7572954daeca3065cc85ec1fc34ba51dc147f220
Gaffle-GH <vinhjrson@gmail.com>
2026-05-19 13:56:27 -0400

    fixed count

--- src/main.py
+++ src/main.py
@@ -1,19 +1,3 @@
 import pyvisa
-<<<<<<< HEAD
 rm = pyvisa.ResourceManager('@py')
 print(rm.list_resources())
-=======
-
-rm = pyvisa.ResourceManager('@py')
-inst = rm.open_resource('ASRL/dev/cu.usbserial-1130::INSTR')
-
-inst.baud_rate = 115200  # XDM2041 uses 115200, not 9600
-inst.data_bits = 8
-inst.stop_bits = pyvisa.constants.StopBits.one
-inst.parity = pyvisa.constants.Parity.none
-inst.timeout = 3000
-inst.read_termination = '\n'
-inst.write_termination = '\n'
-
-print(inst.query('*IDN?'))
->>>>>>> 6b036a3e151fbf9d93af09ded098b787cf5c175c
