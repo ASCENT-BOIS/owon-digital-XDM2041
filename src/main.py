9bba279cfe57ffb2b0eb4162f96a1d677584ac1e
Gaffle-GH <vinhjrson@gmail.com>
2026-05-26 14:18:58 -0400

    ADDED: Packaging

--- src/main.py
+++ src/main.py
@@ -1,7 +1,16 @@
 # The main function, entry to entire program
-from .su_logging import start
+try:
+    # When run as a package: python -m src.main
+    from .su_logging import start
+except ImportError:
+    # When run directly inside src/: python main.py
+    from su_logging import start
 
 
 def main():
     # Run the start program function
     start()
+
+
+if __name__ == "__main__":
+    main()
